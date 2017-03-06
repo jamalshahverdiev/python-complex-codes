e2cc8b658517768e56b1e5050ccd1552e6c62c00
root <jamal.shahverdiev@gmail.com>
2017-03-06 12:17:20 +0400

    changed createstaticmacs.py

--- switch-notificator/createstaticmacs.py
+++ switch-notificator/createstaticmacs.py
@@ -5,8 +5,16 @@ import paramiko
 codepath = os.getcwd()
 outputdir = codepath+'/outdir/'
 
-def get_staticmacs(ip, vlanID):
-    os.system('cat outdir/'+ip+' | grep -v -i ALL | grep '+vlanID+' | awk \'{print $2 }\' >> '+outputdir+'/MAC.list')
+def macget(vlanID, ip):
+    stdin, stdout, stderr = ssh.exec_command('show mac address-table vlan '+vlanID+'')
+    output = stdout.readlines()
+
+    with open(outputdir+''+ip+'', 'wb') as switchout:
+        switchout.write(''.join(output))
+
+def getallMACs(ip, vlanID):
+    os.system('cat '+outputdir+'/'+ip+' | grep -v -i ALL | grep '+vlanID+' | awk \'{print $2 }\' >> '+outputdir+'/MAC.list')
+
 
 ssh = paramiko.SSHClient()
 ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
@@ -14,7 +22,8 @@ ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 iplist = open('iplist', 'r')
 for ip in iplist.readlines():
     ssh.connect(''.join(ip.split()), port=22, username='switchuser', password='switchpass', look_for_keys=False, allow_agent=False)
-    get_staticmacs(''.join(ip.split()), '823')
+    macget('823', ''.join(ip.split()))
+    getallMACs(''.join(ip.split()), '823')
 
 with open(outputdir+'/MAC.list', 'r') as dirtymacs:
     for line in dirtymacs.readlines():
@@ -25,5 +34,6 @@ with open(outputdir+'/MAC.list', 'r') as dirtymacs:
                 macresult.write(line)
                 print('New line is written to the StaticMacs file!!!')
     os.system('rm -rf '+outputdir+'/MAC.list')
+
 iplist.close()
 ssh.close()
